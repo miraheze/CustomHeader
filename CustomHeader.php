4cb6fa19492bde5cd319c6674b42d993b2b9f07f
Brent Laabs <blaabs@perceptyx.com>
2017-05-20 02:40:04 -0700

    Add this to an actual class, flush out extension.json

---
+++ CustomHeader.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * CustomHeader
+ *
+ * Adds an HTML header to a page, right at the beginning of the <body> tag.
+ *
+ *
+ * @author Brent Laabs (Labster)
+ * @authorlink http://www.mediawiki.org/wiki/User:Labster
+ * @copyright Brent Laabs 2017
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+if ( !defined( 'MEDIAWIKI' ) )
+	die( 'This file is a MediaWiki extension, it is not a vaild entry point' );
+
+$wgExtensionCredits[ 'parserhook' ][] = array(
+	'path'            => __FILE__,
+	'name'           => 'CustomHeader',
+	'author'         => 'Brent Laabs (Labster)',
+	'version'        => '1.0',
+);
+
+// And inside a class...
+class CustomHeaderHooks {
+    public static function onSkinTemplateOutputPageBeforeExec( &$skin, &$template ) {
+        global $wgCustomHeaderHTML;
+        // OutputPage::headElement actually creates the opening <body> tag, so this goes right after
+        if ( $wgHeaderExtensionHTML ) {
+            $template->extend( 'headelement', $wgHeaderExtensionHTML );
+        }
+        return true;
+    }
+}
